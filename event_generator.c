5863fda787bde4776c90f880ce2a6fb70d6c2110
Finn Matras <finn.matras@revolve.no>
2017-02-14 14:32:46 +0100

    Started adding to event-generator

---
+++ event_generator.c
@@ -0,0 +1,5 @@
+#include "event_generator.h"
+
+void event_generator(void){
+    for (int current_button )
+}
---
+++ event_generator.h
@@ -0,0 +1,8 @@
+#ifndef __INCLUDE_DRIVER_H__
+#define __INCLUDE_DRIVER_H__
+
+
+void event_generator(void);
+
+
+#endif // #ifndef __INCLUDE_DRIVER_H__
