1af50e3cdfe115117581c3efcc0dfcc6d7333af9
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-02-16 15:26:27 +0100

    Implemented changes in order manager. Added lots of debug messages to monitor progress. #define _DEBUG_MESSAGES_ to enable.

--- order_manager.c
+++ order_manager.c
@@ -12,6 +12,40 @@ typedef struct Order_list_node_t{
 
 static Order_list_node_t* order_queue = NULL;
 
+static void _print_order_queue() {
+	Order_list_node_t* node;
+	
+	int position = 1;
+	
+	printf("Order queue:\n");
+	for (node = order_queue; node != NULL; node = node -> next) {
+		printf("%d - ", position);
+		position++;
+		
+		switch (node -> order.pickup_direction) {
+			case direction_up: printf("Up from "); break;
+			case direction_down: printf("Down from "); break;
+			case direction_none: printf("??? from "); break;
+		}
+		
+		switch (node -> order.pickup_floor) {
+			case floor_1st: printf("1st to "); break;
+			case floor_2nd: printf("2nd to "); break;
+			case floor_3rd: printf("3rd to "); break;
+			case floor_4th: printf("4th to "); break;
+			case floor_unknown: printf("??? to "); break;
+		}
+		
+		switch (node -> order.dropoff_floor) {
+			case floor_1st: printf("1st\n"); break;
+			case floor_2nd: printf("2nd\n"); break;
+			case floor_3rd: printf("3rd\n"); break;
+			case floor_4th: printf("4th\n"); break;
+			case floor_unknown: printf("???\n"); break;
+		}
+	}
+}
+
 static Order_list_node_t* _find_node_with_order (Order_t* order) {
 	if (order_queue == NULL) {
 		return NULL;
@@ -44,7 +78,11 @@ static Order_list_node_t* _create_new_node (Floor_t pickup_floor, Direction_t di
 
 void om_add_new_order (Floor_t pickup_floor, Direction_t direction) {
 	// Check if queue contains this order already
-	if (om_contains_pickup(pickup_floor, direction)) {
+	Order_t* old_order = om_contains_pickup(pickup_floor, direction);
+	if (old_order != NULL && old_order -> dropoff_floor == floor_unknown) {
+#ifdef _DEBUG_MESSAGES_
+		printf("Order already present.\n");
+#endif
 		return;
 	}
 
@@ -53,6 +91,30 @@ void om_add_new_order (Floor_t pickup_floor, Direction_t direction) {
 	
 	// Set the new node as the first in the queue
 	order_queue = new_order_node;
+	
+#ifdef _DEBUG_MESSAGES_
+	_print_order_queue();
+#endif
+}
+
+void om_add_new_dropoff_only_order (Floor_t dropoff_floor) {
+	// Check if queue contains this order already
+	if (om_contains_dropoff(dropoff_floor)) {
+#ifdef _DEBUG_MESSAGES_
+		printf("Dropoff floor already present.\n");
+#endif
+		return;
+	}
+
+	// Create new node with given values
+	Order_list_node_t* new_order_node = _create_new_node(floor_unknown, direction_none, dropoff_floor, order_queue);
+	
+	// Set the new node as the first in the queue
+	order_queue = new_order_node;
+	
+#ifdef _DEBUG_MESSAGES_
+	_print_order_queue();
+#endif
 }
 
 Order_t* om_get_first_order (void) {
@@ -64,7 +126,27 @@ Order_t* om_get_first_order (void) {
 	}
 }
 
-void om_add_dropoff_to_order (Order_t* order, Floor_t dropoff_floor) {
+void om_add_dropoff_to_order (Floor_t pickup_floor, Direction_t direction, Floor_t dropoff_floor) {
+	// Find order with given pick-up floor and direction
+#ifdef _DEBUG_MESSAGES_
+		printf("Looking for order\n");
+#endif
+	Order_list_node_t* node;
+	Order_t* order = NULL;
+	for (node = order_queue; node != NULL; node = node -> next) {
+		if (node -> order.pickup_floor == pickup_floor && node -> order.pickup_direction == direction) {
+			order = node -> order;
+			break;
+		}
+	}
+	
+	if (order == NULL) {
+#ifdef _DEBUG_MESSAGES_
+		printf("No order found\n");
+#endif
+		return;
+	}
+	
 	if (order -> dropoff_floor == floor_unknown) {
 		// Order has no drop-off floor, add it.
 		order -> dropoff_floor = dropoff_floor;
@@ -74,6 +156,11 @@ void om_add_dropoff_to_order (Order_t* order, Floor_t dropoff_floor) {
 		Order_list_node_t* new_node = _create_new_node(order -> pickup_floor, order -> pickup_direction, dropoff_floor, node -> next);
 		node -> next = new_node;
 	}
+	
+#ifdef _DEBUG_MESSAGES_
+	printf("Dropoff floor added successfully!\n");
+	_print_order_queue();
+#endif
 }
 
 void om_remove_order (Order_t* order) {
@@ -87,6 +174,10 @@ void om_remove_order (Order_t* order) {
 		Order_list_node_t* to_delete = order_queue;
 		order_queue = order_queue -> next;
 		free(to_delete);
+#ifdef _DEBUG_MESSAGES_
+		printf("Order removed successfully (was 1st in queue)\n");
+		_print_order_queue();
+#endif
 		return;
 	}
 	
@@ -97,7 +188,10 @@ void om_remove_order (Order_t* order) {
 		node = node -> next;
 		if (node -> next == NULL) {
 			// Order to remove was not found in the queue
-			return;
+			return;	
+#ifdef _DEBUG_MESSAGES_
+			printf("Order to delete was not found\n");
+#endif
 		}
 	}
 	
@@ -105,32 +199,47 @@ void om_remove_order (Order_t* order) {
 	Order_list_node_t* to_delete = node -> next;
 	node -> next = node -> next -> next;
 	free(to_delete);
+	
+#ifdef _DEBUG_MESSAGES_
+	printf("Order removed successfully!\n");
+	_print_order_queue();
+#endif
 }
 
 void om_clear_all_orders (void) {
 	while (order_queue != NULL) {
 		om_remove_order (&(order_queue -> order));
 	}
+#ifdef _DEBUG_MESSAGES_
+	printf("Order queue cleared successfully!\n");
+	_print_order_queue();
+#endif
 }
 
-bool om_contains_pickup (Floor_t pickup_floor, Direction_t direction) {
+Order_t* om_contains_pickup (Floor_t pickup_floor, Direction_t direction) {
 	Order_list_node_t* node;
 	for (node = order_queue ; node != NULL ; node = node -> next) {
-		if (node->order.pickup_floor == pickup_floor && node->order.pickup_direction == direction) {
-			return true;
+		if (node -> order.pickup_floor == pickup_floor && node -> order.pickup_direction == direction) {
+#ifdef _DEBUG_MESSAGES_
+			printf("Found order with pickup!\n");
+#endif
+			return &(node -> order);
 		}
 	}
 	
-	return false;
+	return NULL;
 }
 
-bool om_contains_dropoff (Floor_t dropoff_floor) {
+Order_t* om_contains_dropoff (Floor_t dropoff_floor) {
 	Order_list_node_t* node;
 	for (node = order_queue ; node != NULL ; node = node -> next) {
-		if (node->order.dropoff_floor == dropoff_floor) {
-			return true;
+		if (node -> order.dropoff_floor == dropoff_floor) {
+#ifdef _DEBUG_MESSAGES_
+			printf("Found order with dropoff!\n");
+#endif
+			return &(node -> order);
 		}
 	}
 	
-	return false;
+	return NULL;
 }
