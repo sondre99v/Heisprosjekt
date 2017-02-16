dc534e961c86cd9a282f7262d3551086edb2d741
gitariansen <elsa.ariansen@gmail.com>
2017-02-16 15:14:20 +0100

    Changed interface

--- order_manager.h
+++ order_manager.h
@@ -26,11 +26,12 @@ typedef struct {
 } Order_t;
 
 void om_add_new_order (Floor_t pickup_floor, Direction_t direction);
+void om_add_new_dropoff_only_order (Floor_t dropoff_floor);	//This has to be implemented
 Order_t* om_get_first_order (void);
-void om_add_dropoff_to_order (Order_t* order, Floor_t dropoff_floor);
+void om_add_dropoff_to_order (Floor_t pickup_floor, Direction_t direction, Floor_t dropoff_floor); //Direction not adress
 void om_remove_order (Order_t* order);
 void om_clear_all_orders (void);
-bool om_contains_pickup (Floor_t pickup_floor, Direction_t direction);
+Order_t* om_contains_pickup (Floor_t pickup_floor, Direction_t direction); //This returns the order
 bool om_contains_dropoff (Floor_t dropoff_floor);
 
 #endif // #ifndef __ORDER_MANAGER_H__
